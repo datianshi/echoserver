3d3789d627bf07ed1a4738c468883275c92347d3
datianshi <dsz0111@gmail.com>
2016-09-30 12:52:23 -0500

    add echo server

---
+++ echoserver.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	server, err := net.Listen("tcp", ":"+port)
+	if server == nil {
+		panic("couldn't start listening: " + err.Error())
+	}
+	conns := clientConns(server)
+	for {
+		go handleConn(<-conns)
+	}
+}
+
+func clientConns(listener net.Listener) chan net.Conn {
+	ch := make(chan net.Conn)
+	i := 0
+	go func() {
+		for {
+			client, err := listener.Accept()
+			if client == nil {
+				fmt.Printf("couldn't accept: " + err.Error())
+				continue
+			}
+			i++
+			fmt.Printf("%d: %v <-> %v\n", i, client.LocalAddr(), client.RemoteAddr())
+			ch <- client
+		}
+	}()
+	return ch
+}
+
+func handleConn(client net.Conn) {
+	b := bufio.NewReader(client)
+	for {
+		line, err := b.ReadBytes('\n')
+		if err != nil { // EOF, or worse
+			break
+		}
+		client.Write(line)
+	}
+}
